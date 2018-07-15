91e220f9b6380776871f1593d239cac527128d88
Yahe <hello@yahe.sh>
2018-07-15 23:33:15 +0200

    fixed bracket

--- system/user.php
+++ system/user.php
@@ -146,7 +146,7 @@
       // sort array by content field
       if (usort($sortable,
                 function ($left, $right) use ($field, $comparator) {
-                  return $comparator(value($left, $field), value($right, ($field));
+                  return $comparator(value($left, $field), value($right, $field));
                 })) {
         // remerge previously split elements
         $result = array_merge($sortable, $unsortable);
